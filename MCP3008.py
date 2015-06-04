3027cf3d899500d4b5be54d29df4775fe939b686
Raspberry Pi <richard@dreamsilver.local>
2015-06-04 01:48:55 +0100

    MCP3008 docstrings

--- MCP3008.py
+++ MCP3008.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
-#
-# MCP3008.py
-#
-# Provides an interface (and test code) to the MCP3008 chip
-# This chip is an 8 channel, 10-bit ADC with SPI interface
-#
-# 25-05-2015
-#
+"""
+MCP3008.py
+
+Provides an interface (and test code) to the MCP3008 chip
+This chip is an 8 channel, 10-bit ADC with SPI interface
+
+25-05-2015
+"""
 
 import spidev
 
 class MCP3008:
-    # initialise
+    """ Provides an interface to read analogue data from an MCP3008 chip """
+
     def __init__(self):
+        """ initialise """
         self.default_channel = 0
         self.spi = spidev.SpiDev()
         self.spi.open(0, 0)
 
-    # string representation of the MCP3008 - used for debugging
     def __repr__(self):
+        """ string representation of the MCP3008 - used for debugging """
         return "MCP3008()"
 
-    # setup a default channel to read from
     def set_default_channel(self, channel):
+        """ 
+        setup a default channel to read from 
+        Parameters
+        channel - the new default channel (ignored if < 0 or > 7)
+        """
         if (channel >= 0 and channel <= 7):
             self.default_channel = channel
 
-    # read the 10-bit value from specified channel
-    # code taken from http://www.raspberrypi-spy.co.uk/2013/10/analogue-sensors-on-the-raspberry-pi-using-an-mcp3008/
+    # code taken from http://www.raspberrypi-spy.co.uk/2013/10/
+    #               analogue-sensors-on-the-raspberry-pi-using-an-mcp3008/
     def read_channel(self, channel=-1):
+        """ 
+        read the 10-bit value from specified channel
+        Parameters
+        channel=-1 - the channel to read from. If -1, the default is used
+        """
         if channel == -1:
             channel = self.default_channel
 
@@ -42,6 +53,7 @@ class MCP3008:
 import time
 
 def main():
+    """ test code """
     mcp = MCP3008()
 
     print "[+] outputting mcp"
